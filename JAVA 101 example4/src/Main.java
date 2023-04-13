c07188b135b428a2e720ebe4900a0b081ea6d475
Livanur Nagihan Duman <128521365+livanurnagihanduman@users.noreply.github.com>
2023-04-13 23:55:13 +0300

    Add files via upload

--- JAVA 101 example4/src/Main.java
+++ JAVA 101 example4/src/Main.java
@@ -1,4 +1,59 @@
+import java.util.Scanner;
 public class Main {
+
+    static int topla(int y , int q){
+        int sonuc = y + q;
+        System.out.println("Toplam =  " + sonuc);
+        return sonuc;
+    }
+
+    static int cikar(int y , int q){
+        int sonuc = y - q;
+        System.out.println("Fark = "  + sonuc);
+        return sonuc;
+    }
+
+    static int carp(int y , int q){
+        int sonuc = y * q;
+        System.out.println("Sonuç = " + sonuc);
+        return sonuc;
+    }
+
+    static int bol(int y , int q){
+        int sonuc = y / q;
+        System.out.println("Sonuç = " + sonuc);
+        return sonuc;
+    }
+
+    static int modAl(int y , int q){
+        int sonuc = y % q;
+        System.out.println("Sonuç = " + sonuc);
+        return sonuc;
+    }
+
+    static int dikdötrgenAlanveCevre(int y , int q){
+        int alanSonuc = y * q;
+        int cevreSonuc = 2*y + 2*q;
+        System.out.println("Sonuç = " + alanSonuc);
+        System.out.println("Sonuç = " + cevreSonuc);
+        return alanSonuc;
+    }
+
+    static int silindirinHacmi(int y , int q){
+        int hacim = 3*(y*y)*q;
+        System.out.println("Hacim = " + hacim);
+        return hacim;
+    }
+
+    static int usAlma(int y , int q){
+        int sonuc = 1;
+        for(;q != 0 ; q--){
+            sonuc *= y;
+        }
+        System.out.println("Sonuç = " + sonuc);
+        return sonuc;
+    }
+
 
     static int f(int a) {
         int result = 0;
@@ -32,9 +87,24 @@ public class Main {
         return true;
 
     }
+
+    static int fibo(int x){
+        if(x==1 || x==2){
+            return 1;
+        }
+        return fibo(x-1) + fibo(x-2);
+    }
+
+
     public static void main(String[] args) {
 
         System.out.println(f(5));
+
+
+
+
+
+
 
         //POLİNDROM SAYILARI BULAN PROGRAM
 
@@ -43,6 +113,88 @@ public class Main {
         System.out.println(sonuc);
 
         System.out.println(poindromMu(2459));
+
+
+        // FİBONACCİ PROGRAMI
+
+        System.out.println(fibo(6));
+        System.out.println(fibo(7));
+
+
+
+        // GELİŞMİŞ HESAP MAKİNESİ
+
+        Scanner key = new Scanner(System.in);
+        int sec;
+        System.out.println("Gelişmiş Hesap Makinesi Programına Hoşgeldiniz!");
+        System.out.println("\n");
+
+        String menu = "1.Toplama İşlemi\n" +
+                      "2.Çıkarma İşlemi\n" +
+                      "3.Çarpma İşlemi\n" +
+                      "4.Bölme İşlemi\n" +
+                      "5.Mod Alma\n" +
+                      "6.Dikdörtgenin Alanı ve Çevresi\n" +
+                      "7.Silindirin Hacmi\n" +
+                      "8.Üslü Sayı Hesaplama\n" +
+                      "0.Çıkış\n";
+
+        while (true){
+
+            System.out.println(menu);
+            System.out.println("Bir işlem seçiniz: ");
+            sec = key.nextInt();
+
+            if(sec == 0){
+                break;
+            }
+
+            System.out.println("İlk sayıyı giriniz: ");
+            int y = key.nextInt();
+            System.out.println("İkinci sayıyı seçiniz: ");
+            int q = key.nextInt();
+
+            switch(sec){
+
+                case 1:
+                    topla(y,q);
+                    break;
+
+                case 2:
+                    cikar(y,q);
+                    break;
+
+                case 3:
+                    carp(y,q);
+                    break;
+
+                case 4:
+                    bol(y,q);
+                    break;
+
+                case 5:
+                    modAl(y,q);
+                    break;
+
+                case 6:
+                    dikdötrgenAlanveCevre(y,q);
+                    break;
+
+                case 7:
+                    silindirinHacmi(y,q);
+                    break;
+
+                case 8:
+                    usAlma(y,q);
+                    break;
+
+                default:
+                    System.out.println("Geçersiz bir işlem girdiniz!");
+            }
+        }
+        System.out.println("Hoşçakalın!");
+
+
 
     }
 }
